8f1f6899d549786555b7a65942abe160a23a9706
PAIN <anthoninpain@hotmail.fr>
2023-12-28 15:48:21 +0100

    ajout de publication avec BDD, ajout de champ, utilisation complete de BDD + affichage description

--- src/Controller/meme.php
+++ src/Controller/meme.php
@@ -1,27 +1,26 @@
 <?php
 
-/** @var Twig\Environment $twig 
- * @var int $id
-*/
-
 use Symfony\Component\HttpFoundation\Response;
+use Doctrine\DBAL\Driver\Connection;
+
+/** @var Twig\Environment $twig 
+ * @var Connection $connection
+ * @var int $id
+ */
 
 // Récupération des paramètres GET
 $id = isset($_GET['id']) ? $_GET['id'] : null;
 $memename = isset($_GET['name']) ? $_GET['name'] : null;
 
-$folder = 'memeFile/'; 
-$memes = array();
+// Requête SQL pour récupérer les memes depuis la base de données
+$sql = "SELECT * FROM meme";
+$memes = $connection->fetchAllAssociative($sql);
 
-try {
-    if (is_dir($folder)) {
-        $memes = glob($folder . '*.{jpg,jpeg,png,gif}', GLOB_BRACE);
-    } else {
-        echo 'Le répertoire n\'existe pas.';
-    }
-} catch (Exception $e) {
-    echo 'Exception : ',  $e->getMessage(), "\n";
+// Assurez-vous d'avoir une colonne 'image' dans votre table meme
+foreach ($memes as &$meme) {
+    // Assumez que 'image' est le nom de la colonne où vous stockez les images dans votre table
+    $meme['image'] = base64_encode($meme['image']); // Convertir l'image en base64 pour l'affichage
 }
 
 // Vérifier si les clés existent avant de les utiliser
-return new Response($twig->render('meme/meme.html.twig', ['mem' => $memes, 'id' => $id, 'memename' => $memename]));
+return new Response($twig->render('meme/meme.html.twig', ['memes' => $memes, 'id' => $id, 'memename' => $memename]));
--- src/Controller/postmeme.php
+++ src/Controller/postmeme.php
@@ -1,12 +1,13 @@
 <?php
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 // Initialisation de la base de données SQLite
 $dbPath = dirname(__DIR__) . '/DB/db.sqlite';
 $pdo = new PDO('sqlite:' . $dbPath);
 
-$id= $_GET['id'];
+$id = $_GET['id'];
 
 // Initialisation des erreurs
 $errors = [];
@@ -14,22 +15,44 @@ $errors = [];
 // Traitement du formulaire
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $nom = $_POST['nom'];
+    $description = $_POST['description'];
 
-    $nameFile = $_FILES['image']['name'];
-    $tmpFile = $_FILES['image']['tmp_name'];
-    $typeFile = explode('.', $nameFile)[1];
+    // Vérifier si l'image est correctement uploadée
+    if ($_FILES['image']['error'] === UPLOAD_ERR_OK) {
+        $nameFile = $_FILES['image']['name'];
+        $tmpFile = $_FILES['image']['tmp_name'];
+        $typeFile = pathinfo($nameFile, PATHINFO_EXTENSION);
 
-    $correctType = array("png",'jpg',"gif");
-    $uploadDir = dirname(dirname(__DIR__)) ."\public\memeFile/";
+        $correctType = array("png", 'jpg', "gif");
 
-    if (in_array($typeFile, $correctType)) {
-        echo"correct type file";
-        if (move_uploaded_file($tmpFile,$uploadDir . $nom . "." . $typeFile)) {
-            echo"Uploaded !";
+        if (in_array($typeFile, $correctType)) {
+            // Chemin où l'image sera stockée
+            $uploadDir = dirname(dirname(__DIR__)) . "/public/memeFile/";
+            $uploadPath = $uploadDir . $nom . "." . $typeFile;
+
+            // Déplacer l'image vers le répertoire d'upload
+            if (move_uploaded_file($tmpFile, $uploadPath)) {
+                // Insérer les informations du meme dans la base de données
+                $sql = "INSERT INTO meme (name, description, image, publicationDate) VALUES (:name, :description, :image, :publicationDate)";
+                $stmt = $pdo->prepare($sql);
+                $stmt->bindValue(':name', $nom);
+                $stmt->bindValue(':description', $description);
+                $stmt->bindValue(':image', file_get_contents($uploadPath), PDO::PARAM_LOB);
+                $stmt->bindValue(':publicationDate', date('Y-m-d H:i:s'), PDO::PARAM_STR);
+                $stmt->execute();
+
+                // Rediriger vers la page des memes
+                header("Location: /meme?id={$id}");
+                exit;
+            } else {
+                $errors[] = "Erreur lors de l'upload de l'image.";
+            }
+        } else {
+            $errors[] = "Type de fichier non pris en charge. Utilisez les formats : png, jpg, gif.";
         }
     } else {
-        echo"not correct type";
+        $errors[] = "Erreur lors de l'upload de l'image.";
     }
 }
 
-return new Response($twig->render('meme/postmeme.html.twig', ['errors' => $errors, 'id'=>$id]));
+return new Response($twig->render('meme/postmeme.html.twig', ['errors' => $errors, 'id' => $id]));
