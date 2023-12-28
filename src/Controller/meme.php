aba08ea5a73f0a11d0aa40d237f3e219da400669
Noe <noe.delvaux@free.fr>
2023-12-28 22:28:22 +0100

    Page meme unique fonctionelle

--- src/Controller/meme.php
+++ src/Controller/meme.php
@@ -8,21 +8,37 @@ use Doctrine\DBAL\Driver\Connection;
  * @var int $id
  */
 
+$dbPath = dirname(__DIR__) . '/DB/db.sqlite';
+$pdo = new PDO('sqlite:' . $dbPath);
+
 session_start();
 
 // Récupération des paramètres GET
-$id = isset($_GET['id']) ? $_GET['id'] : null;
-$memename = isset($_GET['name']) ? $_GET['name'] : null;
+$id = isset($_GET['id']) ? $_GET['id'] : null;//id de l'image selectionee
+
 
 // Requête SQL pour récupérer les memes depuis la base de données
-$sql = "SELECT * FROM meme";
-$memes = $connection->fetchAllAssociative($sql);
+if (!$id) {
+    $sql = "SELECT * FROM meme";
+    $memes = $connection->fetchAllAssociative($sql);
 
-// Assurez-vous d'avoir une colonne 'image' dans votre table meme
-foreach ($memes as &$meme) {
-    // Assumez que 'image' est le nom de la colonne où vous stockez les images dans votre table
-    $meme['image'] = base64_encode($meme['image']); // Convertir l'image en base64 pour l'affichage
+    // Assurez-vous d'avoir une colonne 'image' dans votre table meme
+    foreach ($memes as &$meme) {
+        // Assumez que 'image' est le nom de la colonne où vous stockez les images dans votre table
+        $meme['image'] = base64_encode($meme['image']); // Convertir l'image en base64 pour l'affichage
+    }
+} else {
+    $stmt = $pdo->prepare('SELECT * FROM meme WHERE id = :id');
+    $stmt->bindParam(':id', $id);
+    $stmt->execute();
+    $theMeme = $stmt->fetchAll();
+    $theMeme= $theMeme[0];
+    $theMeme['image'] = base64_encode($theMeme['image']);
+
+
 }
 
+
+
 // Vérifier si les clés existent avant de les utiliser
-return new Response($twig->render('meme/meme.html.twig', ['memes' => $memes,'session'=>$_SESSION, 'memename' => $memename]));
+return new Response($twig->render('meme/meme.html.twig', ['memes' => $memes,'session'=>$_SESSION, 'id' => $id,'selectedmeme'=>$theMeme]));
