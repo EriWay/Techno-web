b03ea3f250b2589efc983188eb68b7d2e919d18e
PAIN <anthoninpain@hotmail.fr>
2023-12-28 14:01:03 +0100

    retiré les erreurs lié a la connection

--- src/Controller/connect.php
+++ src/Controller/connect.php
@@ -9,28 +9,45 @@ $pdo = new PDO('sqlite:' . $dbPath);
 // Initialisation des erreurs
 $errors = [];
 
-
-// Traitement du formulaire
+// Vérifiez si la méthode est POST
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $email = $_POST['email'];
     $mot_de_passe = $_POST['mot_de_passe'];
 
-    // Si pas d'erreurs, retourne à l'acceuil
+    // Traitement du formulaire
+    // Si pas d'erreurs, retourne à l'accueil
     if (empty($errors)) {
-        $query = "SELECT nom,prenom,mot_de_passe,id FROM utilisateurs WHERE email = :email";
+        $query = "SELECT nom, prenom, mot_de_passe, id FROM utilisateurs WHERE email = :email";
         $stmt = $pdo->prepare($query);
-        $stmt->bindParam(':email', $email);
-        $stmt->execute();
-        $res = $stmt->fetchAll();
-        $user=array(0=>$res[0][0],1=>$res[0][1],2=>$res[0][2],3=>$res[0][3]);
-        if (password_verify($mot_de_passe,$user[2])) {
-            // Redirigez l'utilisateur vers une autre page après l'inscription réussie
-            return new Response($twig->render('home/home.html.twig',['user' => $user,'id'=> $user[3]]));
 
+        // Vérifiez si la préparation de la requête a réussi
+        if ($stmt) {
+            $stmt->bindParam(':email', $email);
+            $stmt->execute();
+            $res = $stmt->fetchAll();
+
+            // Vérifiez si la requête a retourné des résultats
+            if (!empty($res)) {
+                $user = [
+                    'nom' => $res[0]['nom'],
+                    'prenom' => $res[0]['prenom'],
+                    'mot_de_passe' => $res[0]['mot_de_passe'],
+                    'id' => $res[0]['id']
+                ];
+
+                if (password_verify($mot_de_passe, $user['mot_de_passe'])) {
+                    // Redirigez l'utilisateur vers une autre page après l'inscription réussie
+                    return new Response($twig->render('home/home.html.twig', ['user' => $user, 'id' => $user['id']]));
+                } else {
+                    $errors[] = "L'adresse email ou le mot de passe est invalide";
+                }
+            } else {
+                $errors[] = "L'adresse email ou le mot de passe est invalide";
+            }
         } else {
-            $errors[] = "l'adresse email ou le mot de passe est invalide";
+            // Gestion de l'erreur de préparation de la requête
+            $errors[] = "Erreur de préparation de la requête SQL";
         }
-    
     }
 }
 
